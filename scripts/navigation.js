1e1edce0e556a8d2d25b2ba2723d84b5fd4ee36c
domas <domas.guiga@gmail.com>
2024-12-09 18:23:08 +0200

    update header

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -11,4 +11,29 @@ const scrollFunction = () => {
     } else {
         scrollButton.classList.add("hidden");
     }
-  };
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.main-container'); // All sections
+    const navLinks = document.querySelectorAll('.menu-item'); // Navigation links
+
+    // console.log(navLinks);
+    window.addEventListener('scroll', () => {
+        let current = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+            if (scrollY >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute('id');
+            }
+        });
+  
+        navLinks.forEach(link => {
+            link.classList.remove('current-page');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('current-page');
+            }
+        });
+    });
+});
